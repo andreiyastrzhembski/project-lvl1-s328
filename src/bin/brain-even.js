c76d5264ab162220ffd3f4e77f604471f83deef2
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2018-08-23 14:28:28 +0300

    add src/bin/brain-even.js

---
+++ src/bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import greet, { playEven } from '..';
+
+console.log('\nWelcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".\n');
+greet();
+playEven();
