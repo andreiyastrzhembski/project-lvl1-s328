e973047fdd60746dc7653a77efa1cb06d01da289
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2018-08-24 13:19:23 +0300

    update src/games/playGcd.js

--- src/games/playGcd.js
+++ src/games/playGcd.js
@@ -1,13 +1,28 @@
 import greet, {
-    getRandomInt, getAnswer, checkAnswer, congrat,
-  } from '..';
+  getRandomInt, getAnswer, checkAnswer, congrat,
+} from '..';
+
+const findGcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return findGcd(b, a % b);
+};
 
 const playGcd = () => {
-  const userName = greet('Answer "yes" if number even otherwise answer "no".\n');
+  const userName = greet('Find the greatest common divisor of given numbers.');
   const maxNum = 100;
   const maxAttempts = 3;
   for (let i = 0; i < maxAttempts; i += 1) {
-
+    const a = getRandomInt(maxNum);
+    const b = getRandomInt(maxNum);
+    const question = `${a} ${b}`;
+    const correctAnswer = findGcd(a, b);
+    const answer = getAnswer(question);
+    if (!checkAnswer(answer, correctAnswer, userName)) {
+      return;
+    }
+    congrat(userName);
   }
 };
 
