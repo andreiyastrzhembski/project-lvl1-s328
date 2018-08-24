e426c0da96aa687aca2c9ba3ab6a10f580468385
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2018-08-24 18:26:38 +0300

    update src/games/playBalance.js

--- src/games/playBalance.js
+++ src/games/playBalance.js
@@ -2,9 +2,115 @@ import greet, {
   getRandomInt, getAnswer, checkAnswer, congrat,
 } from '..';
 
+const getMin = (num) => {
+  let number = num;
+  let min = number % 10;
+  while (number > 0) {
+    const current = number % 10;
+    number = Math.floor(number / 10);
+    if (current < min) {
+      min = current;
+    }
+  }
+  return min;
+};
+
+const getMax = (num) => {
+  let number = num;
+  let max = number % 10;
+  while (number > 0) {
+    const current = number % 10;
+    number = Math.floor(number / 10);
+    if (current > max) {
+      max = current;
+    }
+  }
+  return max;
+};
+
+const isBalanced = (num) => {
+  if (getMax(num) - getMin(num) <= 1) {
+    return true;
+  }
+  return false;
+};
+
+const hasRightOrder = (num) => {
+  const number = num;
+  for (let i = 0; i < number.length; i += 1) {
+    if (number[i] > number[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const changeOrder = (num) => {
+  let result = '';
+  const number = num;
+  for (let i = 0; i < number.length; i += 1) {
+    if (number[i] > number[i + 1]) {
+      result += number[i + 1] + number[i];
+      i += 1;
+    } else {
+      result += number[i];
+    }
+  }
+  return result;
+};
+
+const balance = (num) => {
+  let result = '';
+  const number = num.toString();
+  let minIndex;
+  let maxIndex;
+  for (let i = 0; i < number.length; i += 1) {
+    if (num[i] === getMin(num).toString()) {
+      minIndex = i;
+    }
+    if (num[i] === getMax(num).toString()) {
+      maxIndex = i;
+    }
+  }
+  for (let i = 0; i < number.length; i += 1) {
+    if (i === minIndex) {
+      result += (getMin(num) + 1).toString();
+    } else if (i === maxIndex) {
+      result += (getMax(num) - 1).toString();
+    } else {
+      result += number[i];
+    }
+  }
+  return result;
+};
+
+const getBalancedNum = (num) => {
+  let number = num;
+  if (isBalanced(number) && hasRightOrder(number)) {
+    return number;
+  }
+  while (!isBalanced(number)) {
+    number = balance(number);
+  }
+  while (!hasRightOrder(number)) {
+    number = changeOrder(number);
+  }
+  return number;
+};
+
 const playBalance = () => {
-
-
+  const userName = greet('Balance the given number.\n');
+  const maxNum = 10000;
+  const maxAttempts = 3;
+  for (let i = 0; i < maxAttempts; i += 1) {
+    const question = getRandomInt(maxNum);
+    const correctAnswer = getBalancedNum(question);
+    const answer = getAnswer(question);
+    if (!checkAnswer(answer, correctAnswer, userName)) {
+      return;
+    }
+  }
+  congrat(userName);
 };
 
 export default playBalance;
