bfe503fb72c5c25b8300dd3347f3b1573e1e6fb8
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2018-08-24 12:51:53 +0300

    update .gitignore src/bin/brain-gcd.js src/games/playGcd.js

--- src/games/playGcd.js
+++ src/games/playGcd.js
@@ -3,7 +3,12 @@ import greet, {
   } from '..';
 
 const playGcd = () => {
+  const userName = greet('Answer "yes" if number even otherwise answer "no".\n');
+  const maxNum = 100;
+  const maxAttempts = 3;
+  for (let i = 0; i < maxAttempts; i += 1) {
 
+  }
 };
 
 export default playGcd;
