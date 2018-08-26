df2597aef28db0901746172d00668b75d3e34095
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2018-08-26 14:51:56 +0300

    update src/games/playPrime.js

--- src/games/playPrime.js
+++ src/games/playPrime.js
@@ -2,15 +2,22 @@ import { cons } from 'hexlet-pairs';
 import getRandomInt from '../random';
 import launchGame from '../gameEngine';
 
-const gameDescription = 'Is this number prime?';
+const gameDescription = 'Answer "yes" if number prime otherwise answer "no".';
 const maxNum = 100;
 
-const isEven = number => number % 2 === 0;
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num !== 1;
+};
 
 const getQuestion = () => {
-  const question = getRandomInt(maxNum);
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-  return cons(question, correctAnswer);
+  const question = getRandomInt(maxNum, 1);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return cons(`Is this number prime? ${question}`, correctAnswer);
 };
 
 export default () => launchGame(gameDescription, getQuestion);
