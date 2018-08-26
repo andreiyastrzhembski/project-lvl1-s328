1881eba81c1c42d04dfc28f1e29b3c5c57676df1
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2018-08-26 21:37:16 +0300

    update src/games/playPrime.js

--- src/games/playPrime.js
+++ src/games/playPrime.js
@@ -6,16 +6,19 @@ const gameDescription = 'Answer "yes" if number prime otherwise answer "no".';
 const maxNum = 100;
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-  return num !== 1;
+  return true;
 };
 
 const getQuestionAndCorrectAnswer = () => {
-  const question = getRandomInt(maxNum, 1);
+  const question = getRandomInt(maxNum);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return cons(`Is this number prime? ${question}`, correctAnswer);
 };
